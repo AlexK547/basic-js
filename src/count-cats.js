80da3d20e05f510f6ae0145db76e62452ff60520
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-28 09:20:54 +0400

    fix: fixed loop

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,7 +4,7 @@ module.exports = function countCats(matrix) {
   let count = 0;
   
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length) {
+    for (let j = 0; j < matrix[i].length; j++) {
       if (matrix[i][j] === '^^') count++;
     }
   }
